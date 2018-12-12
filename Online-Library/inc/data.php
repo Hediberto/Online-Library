cb0d46a0f4bd78ee58c936184b0742c4d3975fa7
Hediberto <41347989+Hediberto@users.noreply.github.com>
2018-12-12 16:40:26 -0500

    Add files via upload

---
+++ Online-Library/inc/data.php
@@ -0,0 +1,186 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+   "title" => "Holes",
+   "img" => "img/media/holes.jpeg",
+  "genre" => "Adventure, Satire",
+  "format" => "Noval",
+  "year" => 1998,
+  "category" => "Books",
+  "authors" => [
+      "Louis Sachar",
+      "",
+      ""
+  ],
+  "publisher" => "Farrar, Straus and Giroux",
+  "isbn" => '978-0-786-22186-8'
+];
+
+$catalog[102] = [
+   "title" => "The Cat In The Hat",
+   "img" => "img/media/cat.jpeg",
+  "genre" => "Children's literature",
+  "format" => "Barnes & Noble",
+  "year" => 1973,
+  "category" => "Books",
+  "authors" => [
+      "Dr. Seuss",
+      "",
+      ""
+  ],
+  "publisher" => "Penguin Random House LLC., Houghton, Mifflin Company",
+  "isbn" => '978-2-371-00019-3'
+];
+
+$catalog[103] = [
+   "title" => "Green Eggs and Ham",
+   "img" => "img/media/green.jpeg",
+  "genre" => "Children's literature",
+  "format" => "Book",
+  "year" => 1960,
+  "category" => "Books",
+  "authors" => [
+      "Dr. Seuss",
+      "",
+      ""
+  ],
+  "publisher" => "	One Fish Two Fish Red Fish Blue Fish",
+  "isbn" => '978-0-394-80016-5'
+];
+
+$catalog[104] = [
+   "title" => "How The Grinch Stole Christmas",
+   "img" => "img/media/grinch.jpeg",
+  "genre" => "Children's literature",
+  "format" => "Book",
+  "year" => 1957,
+  "category" => "Books",
+  "authors" => [
+      "Dr. Seuss",
+      "",
+      ""
+  ],
+  "publisher" => "	Redbook (magazine)
+  Random House (book)",
+  "isbn" => '0-394-80079-6'
+];
+
+//Movies
+$catalog[201] = [
+  "title" => "Avengers: Infinity War",
+  "img" => "img/media/avengers.jpeg",
+  "genre" => "Action,Adventure,Fantasy",
+  "format" => "DVD",
+  "year" => 2018,
+  "category" => "Movies",
+  "director" => "Anthony Russo",
+  "writers" => [
+      "Stan Lee"
+
+  ],
+  "stars" => [
+      "Robert Downey Jr.",
+      "Chris Hemsworth",
+      "Chris Evans"
+  ]
+];
+
+$catalog[202] = [
+  "title" => "Harry Potter and the Goblet of Fire",
+  "img" => "img/media/harry.jpeg",
+  "genre" => "Action,Adventure,Fantasy",
+  "format" => "DVD",
+  "year" => 2005,
+  "category" => "Movies",
+  "director" => "Mike Newell",
+  "writers" => [
+      "Steve Kloves"
+
+  ],
+  "stars" => [
+      "Daniel Radcliffe",
+      "Rupert Grint",
+      "Emma Watson"
+  ]
+];
+
+$catalog[203] = [
+  "title" => "Star Wars: Episode III - Revenge of the Sith",
+  "img" => "img/media/star.jpeg",
+  "genre" => "Action,Adventure,Fantasy",
+  "format" => "DVD",
+  "year" => 2005,
+  "category" => "Movies",
+  "director" => "George Lucas",
+  "writers" => [
+      "George Lucas"
+
+  ],
+  "stars" => [
+      "Ewan McGregor",
+      "Natalie Portman",
+      "Hayden Christensen"
+  ]
+];
+
+$catalog[204] = [
+  "title" => "Ip Man 4",
+  "img" => "img/media/ipman.jpeg",
+  "genre" => "Action,Fighting,",
+  "format" => "DVD",
+  "year" => 2015,
+  "category" => "Movies",
+  "director" => "Wilson Yip",
+  "writers" => [
+      "Edmond Wong "
+
+  ],
+  "stars" => [
+      "Donnie Yen",
+      "Zhang Jin",
+      "Lynn Hung"
+  ]
+];
+
+//Music
+$catalog[301] = [
+  "title" => "La Revolucion",
+  "img" => "img/media/ la Revolucion.jpeg",
+  "genre" => "Urbano Latino",
+  "format" => "CD",
+  "year" => 2009,
+  "category" => "Music",
+  "artist" => "Wisin Y Yandel"
+];
+
+$catalog[302] = [
+  "title" => "Lideres",
+  "img" => "img/media/lideres.jpeg",
+  "genre" => "Urbano Latino",
+  "format" => "CD",
+  "year" => 2012,
+  "category" => "Music",
+  "artist" => "Wisin Y Yandel"
+];
+
+$catalog[303] = [
+  "title" => "Real Hasta la Muerte",
+  "img" => "img/media/anuel.jpeg",
+  "genre" => "Urbano Latino",
+  "format" => "CD",
+  "year" => 2018,
+  "category" => "Music",
+  "artist" => "Anuel AA"
+];
+
+$catalog[304] = [
+  "title" => "Vibes",
+  "img" => "img/media/vibes.jpg",
+  "genre" => "Urbano Latino",
+  "format" => "CD",
+  "year" => 2018,
+  "category" => "Music",
+  "artist" => "J Balvin"
+];
+?>
